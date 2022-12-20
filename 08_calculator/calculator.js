6c709fd71200ccfa8bee364262fa71c41d047a43
Atharva-Navghane <atharva.navghane@gmail.com>
2022-12-20 22:50:06 +0530

    finished with this exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for(key in arr)
+  {
+    sum = sum + arr[key];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mul = 1;
+  for (key in arr)
+  {
+    mul = mul * arr[key];
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let ans = 1;
+	for (let i = 0;i<b;i++)
+  {
+    ans = ans*a;
+  }
+  return ans;
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if(n ==0 || n==1)
+    return 1;
+  else
+    return n*factorial(n-1);
 	
 };
 
