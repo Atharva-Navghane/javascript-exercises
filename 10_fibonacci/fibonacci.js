db77c597aed5d4d54dd761ecf89c889474a69a62
Atharva-Navghane <atharva.navghane@gmail.com>
2022-12-21 11:55:20 +0530

    finished with this exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+//let ctr = 1;
+const fibonacci = function(a) {
+    if(a >= 0)
+    {
+        if(a == 0)
+            return 0;
+        else if(a == 1)
+            return 1;
+        else
+            return fibonacci(a-1) + fibonacci(a-2);
+    }
+    else
+        return "OOPS";
 
 };
 
